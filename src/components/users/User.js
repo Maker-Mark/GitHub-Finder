bd748392387e8c2bc0590b584fb8ce2d93d6d3bf
Mark <markgoldstein12@gmail.com>
2019-06-29 10:53:48 -0400

    added bio and location on user card

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -31,6 +31,14 @@ export class User extends Component {
             <div className="card grid-2">
                 <div className="all-center">
                     <img src={avatar_url} className="round-img" alt="user-img" style={{width:'150px'}}/>
+                <h1>{name}</h1>
+                <p>Location: {location}</p>
+                </div>
+                <div>
+                    {bio && (<Fragment>
+                            <h3>Bio:</h3>
+                            <p>{bio}</p>
+                        </Fragment>)}
                 </div>
             </div>
             </Fragment>
