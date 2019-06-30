f2315942bcbb0519ecd50b70c269e9df39dedf02
Mark Goldstein <markgoldstein12@gmail.com>
2019-06-30 18:19:36 -0400

    removed extra code

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -100,11 +100,6 @@ export class User extends Component {
           <div className="badge badge-primary">Followers: {followers}</div>
           <div className="badge badge-success">Following: {following}</div>
           <div className="badge badge-light">Public Repos: {public_repos}</div>
-          {/* {public_gists > 0 && (
-            <div className="badge badge-light">
-              Public Gists: {public_gists}
-            </div> 
-          )} */}
           {public_gists > 0 ? (
             <div className="badge badge-dark">Public Gists: {public_gists}</div>
           ) : (
