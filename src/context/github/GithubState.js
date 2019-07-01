74e84863dbd053b6aafb93e1f421336b1526c13d
Mark <markgoldstein12@gmail.com>
2019-07-01 07:17:50 -0400

    added context barebones

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -9,6 +9,27 @@ GET_USER
 CLEAR_USERS
 GET_REPOS
 SET_LOADING
-SET_ALERT
-REMOVE_ALERT
+} from '../types';
+
+const GithubState = props => {
+    // Our global state with anything that has to do with github
+    const initialState = {
+        users:[],
+        user:{},
+        repos: [],
+        loading:false,
+    }
+    //More actions
+    const[state,dispatch]= userReducer(GithubReducer, initialState);
+
+    //Search Users
+
+    //Get User
+
+    //Get Repos
+
+    //Clear Users
+
+    //Set Loading
+
 }
